23e75508ef427f60e45e127d0090a2d3a80ff1c7
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2025-02-05 14:01:58 +0100

    some style changes through the base page and recipe visualization

--- COSE DA FARE.py
+++ COSE DA FARE.py
@@ -2,8 +2,18 @@
 
 #TODO mettere una sezione "cosa ho nel frigo dioboia?" inserendo gli ingredienti fa vedere le ricette che hanno solo quegli ingredienti
 
-#TODO la ricerca va fatta
+#TODO mettere controlli oltre che sullo username in registrazione anche sulla mail (UN solo acount per mail)
+# a che serve la mail? tanto la newsletter per ora non penso sia utile. se si mette la mail dovremmo 
+#come minimo TODO aggiungere la verificazione tramite mail
 
+#TODO nell'add recipe, per gli ingredienti q.b. tanto utilizzato nelle nostre ricette come vogliamo metterlo? Se si frigge qualcosa e c'è scritto 
+# di utiizzare tanto olio quanto basta per coprire il cibo cosa si deeve mettere solo l'ingrediente oppure ci si mette il q.b. anche qua?
+# comunque il q.b. è da mettere, tipo che la quantità è un menù a tendina e ci fa selezionare q.b. o qualcosa del genere.
+
+#TODO lo slider delle servings... oltre al fatto che bisogna impostare meglio la visualizzazione (se il numero è intero mette .00 dopo la virgola)
+# bisogna far si che anche chi ha pubblicato la ricetta possa farlo.
+
+#TODO new entries molto bene, ma si facesse come si è detto anche un Popular/Most rated recipe?
 
 # T O D O di formattazione
 
